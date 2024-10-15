1ecc6aa971fb57b844edb4bcce70aa73ba4d6370
test-github-04 <nastenka.grigoreva@gmail.com>
2024-10-15 23:16:14 +0500

    Обновлены файлы db.properties и UserDaoImp.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -28,10 +28,7 @@ public class MainApp {
             car2.setUser(user2);
             car3.setUser(user3);
             car4.setUser(user4);
-            /*userService.add(user1.setCar(car1).setUser(user1));
-            userService.add(user2.setCar(car2).setUser(user2));
-            userService.add(user3.setCar(car3).setUser(user3));
-            userService.add(user4.setCar(car4).setUser(user4));*/
+
             userService.add(user1);
             userService.add(user2);
             userService.add(user3);
